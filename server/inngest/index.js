2c832c47906a1d92b6847bf82c5a64ba83c0f45c
AtharwaPancholi <pancholiatharwa@gmail.com>
2026-01-02 14:28:47 +0530

    model import fix

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,5 +1,5 @@
-import { User } from "@clerk/express";
 import { Inngest } from "inngest";
+import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
